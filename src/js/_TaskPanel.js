9d584f2fcf40db21fbf54e43078c3dc00eda6bc0
Filip Rudek <rudek.filip@gmail.com>
2020-03-15 21:20:01 +0100

    Renamed panelText varraible. Corrected task's icons RWD

--- src/js/_TaskPanel.js
+++ src/js/_TaskPanel.js
@@ -1,51 +1,45 @@
-import {
-    SingleNote
-} from './_SingleNote';
-import {
-    Storage
-} from './_Storage';
+import { SingleNote } from "./_SingleNote";
+import { Storage } from "./_Storage";
 class TaskPanel {
-    constructor() {
-        this.paneltext = document.getElementById('taskPanelText');
-        this.addBtn = document.querySelector('.addTaskBtn');
-        this.toDoPanel = document.querySelector('.todo_Panel');
-        this.taskStorage = [];
-    }
+  constructor() {
+    this.panelText = document.getElementById("taskPanelText");
+    this.addBtn = document.querySelector(".addTaskBtn");
+    this.toDoPanel = document.querySelector(".todo_Panel");
+    this.taskStorage = [];
+  }
 
-    addTask() {
-        this.addBtn.addEventListener('click', () => {
-            const singleNote = new SingleNote();
-            if (!this.panelTextValidate()) {
-                this.paneltext.classList.add('alert');
-                this.addBtn.classList.add('alert');
-            } else {
-                singleNote.setIndex(singleNote, this.taskStorage);
-                this.paneltext.classList.remove('alert');
-                this.addBtn.classList.remove('alert');
-                this.toDoPanel.appendChild(singleNote.initTask(this.getTaskText()));
-                singleNote.prepareObj(this.taskStorage, singleNote.dateBar.textContent);
-                Storage.setStorage(this.taskStorage);
-                this.setEmptyValue();
-                singleNote.editTask();
-                singleNote.finisTheTask();
-                singleNote.deleteTask(this.taskStorage, singleNote.index);
-            };
-        });
-    };
+  addTask() {
+    this.addBtn.addEventListener("click", () => {
+      const singleNote = new SingleNote();
+      if (!this.panelTextValidate()) {
+        this.panelText.classList.add("alert");
+        this.addBtn.classList.add("alert");
+      } else {
+        singleNote.setIndex(singleNote, this.taskStorage);
+        this.panelText.classList.remove("alert");
+        this.addBtn.classList.remove("alert");
+        this.toDoPanel.appendChild(singleNote.initTask(this.getTaskText()));
+        singleNote.prepareObj(this.taskStorage, singleNote.dateBar.textContent);
+        Storage.setStorage(this.taskStorage);
+        this.setEmptyValue();
+        singleNote.editTask();
+        singleNote.finisTheTask();
+        singleNote.deleteTask(this.taskStorage, singleNote.index);
+      }
+    });
+  }
 
-    getTaskText() {
-        return this.paneltext.value;
-    };
-    setEmptyValue() {
-        return this.paneltext.value = "";
-    };
-    panelTextValidate() {
-        if (this.paneltext.value === "") {
-            return false;
-        } else return true;
-    }
-};
-
-export {
-    TaskPanel
+  getTaskText() {
+    return this.panelText.value;
+  }
+  setEmptyValue() {
+    return (this.panelText.value = "");
+  }
+  panelTextValidate() {
+    if (this.panelText.value === "") {
+      return false;
+    } else return true;
+  }
 }
+
+export { TaskPanel };
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,9 @@
-import '../scss/style.scss';
-import '../images/icons/delete_outline_24px.svg';
-import '../images/icons/done_24px.svg';
-import '../images/icons/mode_24px.svg';
-import {
-    TaskPanel
-} from './_TaskPanel';
-import {
-    Storage
-} from './_Storage';
-
-
+import "../scss/style.scss";
+import "../images/icons/delete_outline_24px.svg";
+import "../images/icons/done_24px.svg";
+import "../images/icons/mode_24px.svg";
+import { TaskPanel } from "./_TaskPanel";
+import { Storage } from "./_Storage";
 
 const taskPanel = new TaskPanel();
 taskPanel.addTask();
