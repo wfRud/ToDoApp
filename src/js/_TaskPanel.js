f956e028eedd6a1b12c3e3ec31dea46aa3df7984
Filip Rudek <rudek.filip@gmail.com>
2020-03-20 16:16:14 +0100

    Fix editing searched task

--- src/js/_TaskPanel.js
+++ src/js/_TaskPanel.js
@@ -15,9 +15,10 @@ class TaskPanel {
         this.panelText.classList.add("alert");
         this.addBtn.classList.add("alert");
       } else {
-        const singleNote = new SingleNote();
         this.panelText.classList.remove("alert");
         this.addBtn.classList.remove("alert");
+
+        const singleNote = new SingleNote();
         this.tasksContainer.appendChild(
           singleNote.initTask(this.getTaskText())
         );
@@ -53,34 +54,31 @@ class TaskPanel {
   searching() {
     // Get tasks text from storage to compare with search input value
     const tasksContent = () => {
-      const tasks = Storage.getStorage();
+      const tasks = Storage.taskStorage();
       return tasks.map(element => {
-        return { noteText: element.noteText, index: element.index };
+        return element;
       });
     };
 
     this.searchInput.addEventListener("input", () => {
       const values = tasksContent();
+      // console.log(values);
 
       // Clear tasks list
       this.tasksContainer.textContent = "";
 
       // Compare each task with input value
       const searchedItem = task => {
-        return task.noteText.includes(this.getSearchText());
+        return task.taskText.value.includes(this.getSearchText());
       };
 
       // Get array of match elements
       const searched = values.filter(searchedItem);
+      // console.log(searched);
 
       // Render match elements
-      searched.forEach(item => {
-        const singleNote = new SingleNote();
-        this.tasksContainer.appendChild(singleNote.initTask(item.noteText));
-        singleNote.index = item.index;
-        singleNote.editTask();
-        singleNote.finishTheTask();
-        singleNote.deleteTask(Storage.taskStorage());
+      searched.map(item => {
+        this.tasksContainer.appendChild(item.singleTask);
       });
     });
   }
