0f4deb7251aabba3287f4e481d1790967daf1321
Filip Rudek <rudek.filip@gmail.com>
2021-10-06 16:15:10 +0200

    Deleted unnecessary imports

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 import "../scss/style.scss";
-import "../images/icons/delete_outline_24px.svg";
-import "../images/icons/done_24px.svg";
-import "../images/icons/mode_24px.svg";
+
 import { TaskPanel } from "./_TaskPanel";
 import { Storage } from "./_Storage";
 
